d96c6658d93176b1b8a71b037b274c08211307ba
Shyam <ssrshyam59@gmail.com>
2023-03-12 16:49:41 +0530

    delete

--- Git Notes/5 Moving and Renaming Files In Git.js
+++
@@ -1,7 +0,0 @@
-// Commands for Moving and Renaming Files In Git:
-
-// 1) git rm first.txt : isse file remove ho jaayegi.
-
-// 2) git mv first.txt first_renamed.txt : isse file ka name rename ho jaayega and iske sath sath yeh remaned file staging area me bhi aa jaayegi.
-
-// 3) git rm --cached file_name : yeh command file ko untrack kar dega jo pehle track ho rahi thi usko, yeh track karna stop kar dega, yeh file ko remove nahi karega sirf untrack kar dega.
--- Git Notes/7 Unstaging & Unmodifying Files In Git.js
+++
@@ -1,7 +0,0 @@
-// Unstaging & Unmodifying Files In Git:
-
-// 1) git restore --staged file_name : Kisi bhi file ko stage se unstage karne ke liye is command ka use karte hai.
-
-// 2) git checkout -- file_name : Agar aapne kisi ek file me kuch change kar diya hai matlab file ko modified kar diya hai and then aapko lagta hai ki kuch galat ho gaya to is command ka use karne se ham pichla wala jo commit haga us par wapas chale jaayenge, or jo naye changes kiye honge bho remove ho jaayenge.
-
-// git checkout -- f : Is command ka use karne se ham sabhi files me jo pichla wala commit hoga us par wapas chale jaayenge, or jo naye changes kiye honge bho remove ho jaayenge (matlab agar aapko apne modified changes remove karke last waale commit me wapas jana hai tab is command ka us kare).
